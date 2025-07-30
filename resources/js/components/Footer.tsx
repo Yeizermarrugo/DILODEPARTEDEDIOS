1c13bf158eaf60928d3bdd8658fa5f759fe1efbf
Yeizermarrugo <yeizermarrugo@gmail.com>
2025-07-29 22:10:57 -0500

    [ADD] footer components

---
+++ resources/js/components/Footer.tsx
@@ -0,0 +1,17 @@
+const Footer = () => {
+    return (
+        <footer id="footer" className="footer">
+            <div className="footer-top container">{/* ...Footer content as JSX... */}</div>
+            <div className="copyright container mt-4 text-center">
+                <p>
+                    © <span>Copyright</span> <strong className="sitename px-1">Dilo de parte de Dios</strong> <span>All Rights Reserved</span>
+                </p>
+                <div className="credits">
+                    Designed by <a href="">The Glory of God</a>
+                </div>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
