47522b55fdc70a1ac2d50c76f438d2a9e9fe4527
Yeizermarrugo <yeizermarrugo@gmail.com>
2025-08-20 13:03:28 -0500

    Update footer design and branding

--- resources/js/components/Footer.tsx
+++ resources/js/components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
             <div className="footer-top container">{/* ...Footer content as JSX... */}</div>
             <div className="copyright container mt-4 text-center">
                 <p>
-                    © <span>Copyright</span> <strong className="sitename px-1">Dilo de parte de Dios</strong> <span>All Rights Reserved</span>
+                    © <span>Copyright</span> <strong className="sitename px-1">Casa de Valientes</strong> <span>All Rights Reserved</span>
                 </p>
                 <div className="credits">
                     Designed by <a href="">The Glory of God</a>
