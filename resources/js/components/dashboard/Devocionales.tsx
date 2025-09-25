63373d14facd65b061cbd00a8acda7e2bd2c6bdd
Yeizermarrugo <yeizermarrugo@gmail.com>
2025-09-25 14:11:26 -0500

    Reorganize the "Add Devotionals" link within the Devotionals component

--- resources/js/components/dashboard/Devocionales.tsx
+++ resources/js/components/dashboard/Devocionales.tsx
@@ -9,21 +9,21 @@ const Devocionales = ({ className }: DevocionalesProps) => {
         <>
             <div className={className}>
                 <div className="blog-hero" data-aos="fade-up" data-aos-delay="100" style={{ width: '100%', height: '100%', padding: '0' }}>
-                    <article className="blog-item featured" data-aos="fade-up">
-                        <img src="/assets/img/blog/Devocionales.jpg" alt="Blog Image" className="img-fluid" />
-                        <div className="blog-content">
-                            <div className="post-meta">
-                                <span className="date">
-                                    {new Date().toLocaleDateString('es-ES', { day: '2-digit', month: 'long', year: 'numeric' })}
+                    <a href="/devocionalesAgregar" title="Devocionales - Daily Inspirations">
+                        <article className="blog-item featured" data-aos="fade-up">
+                            <img src="/assets/img/blog/Devocionales.jpg" alt="Blog Image" className="img-fluid" />
+                            <div className="blog-content">
+                                <div className="post-meta">
+                                    <span className="date">
+                                        {new Date().toLocaleDateString('es-ES', { day: '2-digit', month: 'long', year: 'numeric' })}
+                                    </span>
+                                </div>
+                                <span className="post-meta">
+                                    <h2 style={{ color: '#f75815' }}>Agregar Devocionales</h2>
                                 </span>
                             </div>
-                            <span className="post-meta">
-                                <a href="/devocionalesAgregar" title="Devocionales - Daily Inspirations">
-                                    Agregar Devocionales
-                                </a>
-                            </span>
-                        </div>
-                    </article>
+                        </article>
+                    </a>
                 </div>
             </div>
         </>
