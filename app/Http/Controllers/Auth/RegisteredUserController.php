d94dc587168ff51ec83d78dffe59bc89c29bc9e8
yeizermarrugo <yeizermarrugo@gmail.com>
2026-03-04 12:21:14 -0500

    Disable automatic login after registration and redirect to the login page

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -44,8 +44,8 @@ class RegisteredUserController extends Controller
 
         event(new Registered($user));
 
-        Auth::login($user);
+        // Auth::login($user);
 
-        return redirect()->intended(route('dashboard', absolute: false));
+        return redirect()->intended(route('login'));
     }
 }
