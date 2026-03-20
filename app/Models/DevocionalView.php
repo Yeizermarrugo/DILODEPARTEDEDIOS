a973b1f3f1f453265ff2c21e2bc5c984ce116e70
yeizermarrugo <yeizermarrugo@gmail.com>
2026-03-19 21:01:58 -0500

    Agregar campos 'browser' y 'accepted_terms' al modelo DevocionalView

--- app/Models/DevocionalView.php
+++ app/Models/DevocionalView.php
@@ -6,6 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class DevocionalView extends Model
 {
-    protected
-        $fillable = ['devocional_id', 'ip_address', 'country', 'platform'];
+    protected $fillable = [
+        'devocional_id',
+        'ip_address',
+        'country',
+        'browser',
+        'platform',
+        'accepted_terms',
+        'local_time',
+        'created_at', // Agrégalo aquí también
+        'updated_at'  // Agrégalo aquí también
+    ];
+
+    // Esto asegura que Laravel trate a created_at como fecha aunque lo mandemos manual
+    protected $casts = [
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
 }
