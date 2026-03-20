c9ae8cecf5f4e9e41c055c9892e7fdbb57f741f1
yeizermarrugo <yeizermarrugo@gmail.com>
2026-03-19 21:19:00 -0500

    Actualizar mensaje de aceptación en el banner de privacidad para incluir "Política de Privacidad"

--- resources/js/components/PrivacyBanner.tsx
+++ resources/js/components/PrivacyBanner.tsx
@@ -47,9 +47,9 @@ const PrivacyBanner = () => {
                     En <strong>Dilo de parte de Dios</strong>, valoramos tu privacidad.
                     Utilizamos registros anónimos (como tu ubicación general y tipo de dispositivo)
                     para entender el alcance de nuestros devocionales, estudios biblicos, enseñanzas y mejorar tu experiencia.
-                    Al continuar, aceptas nuestra
+                    Al continuar, aceptas nuestra Política de Privacidad
                     {/* <a href="/privacidad" style={{ color: '#77d7b9', textDecoration: 'underline' }}> */}
-                    Política de Privacidad
+
                     {/* </a>. */}
                 </p>
             </div>
