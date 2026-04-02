ea5087faf4bd3c22bf222c0fe1550c6721f4bb43
yeizermarrugo <yeizermarrugo@gmail.com>
2026-04-02 15:11:58 -0500

    Agregar rutas para la API de likes en api.php

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BulkUploadController;
+use App\Http\Controllers\LikeController;
 
 Route::post('/bulk-upload', [BulkUploadController::class, 'store']);
 Route::get('/bucket/images', [BulkUploadController::class, 'index']);
+
+//-------- Likes API ---------
+Route::get('/likes/{type}/{id}',  [LikeController::class, 'show']);
+Route::post('/likes/{type}/{id}', [LikeController::class, 'toggle']);
