5f5b8b6c1d2ea6e935cc2fdbd898ea45f79b0b60
yeizermarrugo <yeizermarrugo@gmail.com>
2026-04-06 14:40:12 -0500

    feat: eliminar ruta para la página offline

--- routes/web.php
+++ routes/web.php
@@ -233,10 +233,5 @@ Route::get('/gracias', function () {
     return inertia('ThanksPage');
 })->name('payment.thanks');
 
-
-Route::get('/offline', function () {
-    return Inertia::render('Offline');
-});
-
 require __DIR__ . '/settings.php';
 require __DIR__ . '/auth.php';
